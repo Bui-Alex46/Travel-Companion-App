607f0c74f8443e9548c1637b226d29b73a6fcbf3
Topia99 <1242911982z@gmail.com>
2023-11-15 15:52:54 -0800

    modify table

--- back_end/travel_companion_db.py
+++ back_end/travel_companion_db.py
@@ -5,13 +5,27 @@ conn = sqlite3.connect('travel_companion.db')
 
 c = conn.cursor()
 
-
-statement = '''CREATE TABLE IF NOT EXISTS favorites (
-            name        TEXT    NOT NULL,
-            address     TEXT    NOT NULL
+create_favorites = '''CREATE TABLE IF NOT EXISTS favorites (
+            id          INTEGER     NOT NULL PRIMARY KEY,
+            name        TEXT        NOT NULL,
+            addressID   INTEGER     NOT NULL UNIQUE,
+            FOREIGN KEY(addressID) REFERENCES address(id)
     );'''
 
-c.execute(statement)
+drop_favorites = '''DROP TABLE favorites;'''
+
+
+create_address = '''CREATE TABLE IF NOT EXISTS address (
+            id          INTEGER     NOT NULL PRIMARY KEY,
+            street      TEXT        NOT NULL,
+            city        TEXT        NOT NULL,
+            state       TEXT        NOT NULL 
+    );'''
+
+
+
+
+c.execute(create_address)
 
 conn.commit()
 
